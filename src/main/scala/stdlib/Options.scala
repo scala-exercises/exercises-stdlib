9c5f115fb16708cd01f192f4e3344b409f6ab304
Emma Burrows <emma-burrows@users.noreply.github.com>
2018-05-31 21:09:00 +0100

    Clarify the purpose of getOrElse

--- src/main/scala/stdlib/Options.scala
+++ src/main/scala/stdlib/Options.scala
@@ -35,7 +35,7 @@ object Options extends FlatSpec with Matchers with org.scalaexercises.definition
    * }
    * }}}
    *
-   * Using `getOrElse` we can provide a default value ("No value") when the optional argument (`None`) does not exist:
+   * Using `getOrElse`, we can extract the value if the `Option[A]` is a `Some[A]`, and provide a default value ("No value") when the optional argument (`None`) does not exist:
    */
   def getOrElseOptions(res0: String, res1: String, res2: String) {
     val value1 = maybeItWillReturnSomething(true)
