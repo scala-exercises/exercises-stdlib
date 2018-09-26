57f992a8b9cecc389d78d732e9252d33612e8f59
Waldemar Wosiński <kastoestoramadus@gmail.com>
2018-09-26 16:11:34 +0100

    Update TypeVariance.scala
    Fruits weren't listed

--- src/main/scala/stdlib/TypeVariance.scala
+++ src/main/scala/stdlib/TypeVariance.scala
@@ -19,6 +19,16 @@ object TypeVariance extends FlatSpec with Matchers with org.scalaexercises.defin
    * Scala's type inference system works a little differently, but it's similar in spirit: infer constraints, and attempt to unify a type.
    *
    * Using type inference the type that you instantiate will be the val or var reference type:
+   *
+   * {{{
+   * class Fruit
+   * abstract class Citrus extends Fruit
+   * class Orange          extends Citrus
+   * class Tangelo         extends Citrus
+   * class Apple           extends Fruit
+   * class Banana          extends Fruit
+   * }}}
+   *
    */
   def syntacticOverheadTypeVariance(res0: String) {
     class MyContainer[A](val a: A)(implicit manifest: scala.reflect.Manifest[A]) {
